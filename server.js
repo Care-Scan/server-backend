01859d47311ed75233f9ccd35db0b23b930a80a5
jae <albertforus@gmail.com>
2023-02-11 16:38:39 -0500

    upload workflow from html

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const formidable = require('formidable');
+ 
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : Resolve to your project folder.
+});
+
+app.post('/api/upload', (req, res, next) => {
+    const form = formidable({ });
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      console.log(files.upload.filepath); 
+      run_gc_vision(files.upload.filepath);
+    });
+
+    
+  });
+
+app.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000 ...');
+});
+
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-/**
- * TODO(developer): Uncomment the following line before running the sample.
- */
 // const fileName = 'Local image file, e.g. /path/to/image.png';
 
 // Read a local image as a text document
-async function start() {
-const [result] = await client.documentTextDetection("sample.png");
+async function run_gc_vision(filename) {
+const [result] = await client.documentTextDetection(filename);
 const fullTextAnnotation = result.fullTextAnnotation;
 console.log(`Full text: ${fullTextAnnotation.text}`);
 }
-
-start()
