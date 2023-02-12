956422f761acb9996e409a0ee2d3a195ae441608
jae <albertforus@gmail.com>
2023-02-12 02:07:44 -0500

    serve image with formidable name settings

--- server.js
+++ server.js
@@ -2,21 +2,28 @@ const express = require('express');
 const app = express();
 const formidable = require('formidable');
 const service = require('./services/gc_vision')
-app.use(express.static('public'))
+const path = require('path');
+const uploadFolder = path.join(__dirname, "public");
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/api", (req, res) => {
     res.send({ message: "Hello from server!" });
   });
 
 app.post('/api/upload', (req, res, next) => {
-    const form = formidable({});
+    const form = formidable({ uploadDir: uploadFolder,
+                              keepExtensions: true,
+                              filename: (function (name, ext, part, form){return "test.png"}),
+                              });
+
     form.parse(req, (err, fields, files) => {
         if (err) {
             next(err);
             return;
         }
         console.log(files.upload.filepath);
-        service.run_gc_vision(files.upload.filepath);
+        // service.run_gc_vision(files.upload.filepath);
     });
 });
 
