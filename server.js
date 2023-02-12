b55bc9877d985ae02fd294d0b6e9e190fe64a5ad
jae <albertforus@gmail.com>
2023-02-11 21:26:37 -0500

    serve static folder

--- server.js
+++ server.js
@@ -2,28 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const formidable = require('formidable');
- 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : Resolve to your project folder.
-});
+
+app.use(express.static('public'))
 
 app.post('/api/upload', (req, res, next) => {
-    const form = formidable({ });
+    const form = formidable({});
     form.parse(req, (err, fields, files) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      console.log(files.upload.filepath); 
-      run_gc_vision(files.upload.filepath);
+        if (err) {
+            next(err);
+            return;
+        }
+        console.log(files.upload.filepath);
+        run_gc_vision(files.upload.filepath);
     });
-
-    
-  });
+});
 
 app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000 ...');
+    console.log('Server listening on http://localhost:3000 ...');
 });
 
 // Imports the Google Cloud client library
@@ -32,11 +27,11 @@ const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient();
 
-// const fileName = 'Local image file, e.g. /path/to/image.png';
+// const filename = 'Local image file, e.g. /path/to/image.png';
 
 // Read a local image as a text document
 async function run_gc_vision(filename) {
-const [result] = await client.documentTextDetection(filename);
-const fullTextAnnotation = result.fullTextAnnotation;
-console.log(`Full text: ${fullTextAnnotation.text}`);
+    const [result] = await client.documentTextDetection(filename);
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log(`Full text: ${fullTextAnnotation.text}`);
 }
